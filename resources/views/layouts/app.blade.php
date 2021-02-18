c717332da2a3683b879fb6f37471fc9ae4159d0f
asifweb <asifwebhero@gmail.com>
2021-02-18 18:11:47 +0530

    Data tables with columns visibility features

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,15 +7,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel 8 CRUD with Datatables</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    {{-- Data Tables --}}
+    {{-- Data Tables CSS --}}
         <link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet">
         <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
     {{-- JQUERY --}}
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.js"></script>
-        <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    {{-- Data Tables JS --}}
+        <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.colVis.min.js"></script>
 </head>
 <body>
     @include('layouts.header')
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -12,12 +12,42 @@
         border-radius: 10px;
         box-shadow: 0px 4px 40px -1px #000;
     }
+    .dt-button-collection button {
+        display: block;
+        background: #593196!important;
+        color: #fff;
+        border: none;
+        margin: 5px 0px;
+        width: 108px;
+        text-align: left;
+        border-radius: 5px;
+    }
+    .dt-button-collection {
+        margin-top: 0px !important;
+        position: absolute;
+        background: #f3ecec;
+        padding: 10px;
+        z-index: 2;
+    }
+    .buttons-colvis {
+        display: block;
+        background: #593196!important;
+        color: #fff;
+        border: none;
+        margin: 0px 0px;
+        border-radius: 5px;
+        padding:5px;
+        text-transform:capitalize;
+    }
+    .dt-buttons {
+        display:inline-block;
+    }
 </style>
     <div class="row">
         <div class="col col-sm-6">
-            <a href="{{ url('/product/create') }}" class="btn btn-primary">Add New Product</a>
         </div>
         <div class="col col-sm-6">
+            <a href="{{ url('/product/create') }}" class="btn btn-primary float-right mb-3">Add New Product</a>
         </div>
     </div>
     <table class="table table-hover table-striped table-bordered data-table mt-4">
@@ -37,7 +67,10 @@
     <script>
         $(function() {
             var table = $('.data-table').DataTable({
-                
+                dom: 'Bfrtip',
+                buttons: [
+                    'colvis'
+                ],
                 processing: true,
                 serverSide: true,
                 ajax: "{{ url('product') }}",
